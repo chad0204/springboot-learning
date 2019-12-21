d2f3a2c5f06685606179d07482a9690ba17e10ca
pengchao <2389367080@qq.com>
2019-12-21 14:25:19 +0800

    annotation

--- service-hystrix/src/main/java/com/pc/servicehystrix/ServiceHystrixApplication.java
+++ service-hystrix/src/main/java/com/pc/servicehystrix/ServiceHystrixApplication.java
@@ -15,6 +15,9 @@ import org.springframework.web.client.RestTemplate;
  * 本服务是在restTemplate+ribbon上开启熔断功能
  * 如果在feign上使用，不需要引入依赖和显示开启，只需要配置文件中配置即可feign.hystrix.enabled=true
  *
+ *
+ * 熔断的情况：服务断开，服务超时(默认2000ms)
+ *
  */
 @SpringBootApplication
 @EnableEurekaClient
